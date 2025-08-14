401ec0232e73ad973b90403410ddfc915b93a884
shikharshikharshikhar <34400575+shikharshikharshikhar@users.noreply.github.com>
2025-08-14 17:32:24 -0400

    Update ContactDB.js

--- ContactDB.js
+++ ContactDB.js
@@ -137,7 +137,10 @@ class ContactDB {
   }
 
   async updateContact(id, contact) {
-    await this.runAsync(`
+    console.log('Updating contact ID:', id);
+    console.log('Update data:', contact);
+    
+    const result = await this.runAsync(`
       UPDATE contacts SET
       first_name = ?, last_name = ?, phone_number = ?, email_address = ?,
       street = ?, city = ?, state = ?, zip = ?, country = ?,
@@ -157,6 +160,9 @@ class ContactDB {
       contact.contact_by_phone ? 1 : 0,
       id
     ]);
+    
+    console.log('Update result:', result);
+    return result;
   }
 
   async deleteContact(id) {
